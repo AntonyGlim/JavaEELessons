c39db450e0f3710bf8fdf9b9440d05ddf34a06fd
Antony.Glim <antony.glim@gmail.com>
2020-05-31 16:27:41 +0300

    Add error 404 and 403

---
+++ first-ee-app/src/main/java/glim/antony/lesson1/servlets/errors/Error404.java
@@ -0,0 +1,16 @@
+package glim.antony.lesson1.servlets.errors;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "error404", urlPatterns = "/error404")
+public class Error404 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>You are on page 404</h1>");
+    }
+}
